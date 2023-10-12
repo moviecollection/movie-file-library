41216f55681bc0b47fa44d326bab34f9779d3278
Peyman M <16763358+peymanr34@users.noreply.github.com>
2023-10-12 22:45:38 +0330

    Update the demo project.

--- Source/Demo/Program.cs
+++ Source/Demo/Program.cs
@@ -1,50 +1,47 @@
 ﻿using System;
 
-namespace Demo
+// Create a new instance of the MovieDetector class.
+var movieDetector = new MovieFileLibrary.MovieDetector();
+
+// Sample files.
+string[] files =
 {
-    class Program
+    "D:\\Top Gear 17x03 HDTV.mp4",
+    "D:\\Doctor Who 2005 S09E05 720p.mkv",
+    "D:\\True.Detective.S03E06.720p.x264.mkv",
+    "D:\\Monty.Pythons.Flying.Circus.S03.E04.avi",
+    "D:\\Bohemian.Rhapsody.2018.720p.BRRip.x265.mkv",
+    "D:\\The.Grand.Tour.S02E05.1080p.WEB-DL.6CH.x265.HEVC.mkv",
+    "D:\\Amelie.2001.1080p.BluRay.6CH.x265.HEVC.{imdb-tt0211915}.mkv",
+    "D:\\No.Time.to.Die.2021.1080p.10bit.BluRay.x265.[imdbid-tt2382320].mkv",
+};
+
+foreach (var file in files)
+{
+    // Get information from file name.
+    MovieFileLibrary.MovieFile movieFile = movieDetector.GetInfo(file);
+
+    Console.WriteLine($"Title: {movieFile.Title}");
+    Console.WriteLine($"Year: {movieFile.Year}");
+
+    if (movieFile.IsSeries)
     {
-        static void Main()
-        {
-            // Create a new instance of the MovieDetector class.
-            var movieDetector = new MovieFileLibrary.MovieDetector();
-
-            // Sample files.
-            string[] files =
-            {
-                "D:\\Doctor Who 2005 S09E05 720p.mkv",
-                "D:\\True.Detective.S03E06.720p.x264.mkv",
-                "D:\\Monty.Pythons.Flying.Circus.S03.E04.avi",
-                "D:\\The.Grand.Tour.S02E05.1080p.WEB-DL.6CH.x265.HEVC.mkv",
-                "D:\\Bohemian.Rhapsody.2018.720p.BRRip.x265.mkv",
-                "D:\\Top Gear 17x03 HDTV.mp4"
-            };
-
-            foreach (var file in files)
-            {
-                // Get information from file name.
-                MovieFileLibrary.MovieFile movieFile = movieDetector.GetInfo(file);
-
-                Console.WriteLine("Title: {0}", movieFile.Title);
-                Console.WriteLine("Year: {0}", movieFile.Year);
-
-                if (movieFile.IsSeries)
-                {
-                    Console.WriteLine("IsSeries: {0}", movieFile.IsSeries);
-                    Console.WriteLine("Season: {0}", movieFile.Season);
-                    Console.WriteLine("Episode: {0}", movieFile.Episode);
-                }
-
-                Console.WriteLine("Path: {0}", movieFile.Path);
-                Console.WriteLine("Extension: {0}", movieFile.Extension);
-                Console.WriteLine("IsSuccess: {0}", movieFile.IsSuccess);
-
-                Console.WriteLine("--------------------------------------------");
-            }
-
-            // Wait for user to press a key to exit.
-            Console.WriteLine("Press Any Key To Exit...");
-            Console.ReadKey();
-        }
+        Console.WriteLine($"IsSeries: {movieFile.IsSeries}");
+        Console.WriteLine($"Season: {movieFile.Season}");
+        Console.WriteLine($"Episode: {movieFile.Episode}");
     }
+    else
+    {
+        Console.WriteLine($"ImdbId: {movieFile.ImdbId}");
+    }
+
+    Console.WriteLine($"Path: {movieFile.Path}");
+    Console.WriteLine($"Extension: {movieFile.Extension}");
+    Console.WriteLine($"IsSuccess: {movieFile.IsSuccess}");
+
+    Console.WriteLine("--------------------------------------------");
 }
+
+// Wait for user to press a key to exit.
+Console.WriteLine("Press Any Key To Exit...");
+Console.ReadKey();
